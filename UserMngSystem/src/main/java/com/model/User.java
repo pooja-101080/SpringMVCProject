cad4e8d17556da20a6c78436ed5787f03ccf18ee
pooja-101080 <poojagunjal474@gmail.com>
2025-06-06 15:09:29 +0530

    Add files via upload

---
+++ UserMngSystem/src/main/java/com/model/User.java
@@ -0,0 +1,67 @@
+package com.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="`user`")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int uid;
+	private String uname;
+	private String uaddress;
+    private String username;
+    private String password;
+    private String gender;
+	public int getUid() {
+		return uid;
+	}
+	public void setUid(int uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getUaddress() {
+		return uaddress;
+	}
+	public void setUaddress(String uaddress) {
+		this.uaddress = uaddress;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return "User [uid=" + uid + ", uname=" + uname + ", uaddress=" + uaddress + ", username=" + username
+				+ ", password=" + password + ", gender=" + gender + "]";
+	}
+
+	
+
+	
+}
